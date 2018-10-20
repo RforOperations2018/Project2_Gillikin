98cd1a7211c64df2d318eea8429a1f12eea8ec6b
gillikin <bgilliki@andrew.cmu.edu>
2018-10-19 22:04:01 -0400

    Finish copy/pasting in all the boilerplate (esp. map stuff)

--- Project2_Gillikin/app.R
+++ Project2_Gillikin/app.R
@@ -63,19 +63,25 @@ ui <- fluidPage(
     # Tabset Main Panel
     mainPanel(
       tabsetPanel(
-        tabPanel("Line Plot",
+        tabPanel("Map",
+                 leafletOutput("map")),
+        tabPanel("By Ward",
+                 plotlyOutput("barChart"),
+        tabPanel("Over time",
                  plotlyOutput("linePlot")),
-        tabPanel("Open/Closed",
-                 plotlyOutput("barChart")),
-        tabPanel("Open/Closed",
-                 plotlyOutput("barChart")),
+        tabPanel("Table",
+                 inputPanel(
+                   downloadButton("downloadData","Download Revenue/Expense Data")
+                 ),
+                 fluidPage(DT::dataTableOutput("table"))
+        )
       )
     )
   )
 )
 
 # Define server logic
-server <- function(input, output) {
+server <- function(input, output, session = session) {
   load311 <- reactive({
     # Build API Query with proper encodes
     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$dates[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$dates[2], "%27%20AND%20%22REQUEST_TYPE%22%20=%20%27", input$type_select, "%27")
@@ -87,21 +93,29 @@ server <- function(input, output) {
     
     return(dat311)
   })
-  output$linePlot <- renderPlotly({
-    dat311 <- load311()
-    
-    # shape the data for chart
-    table <- dat311 %>%
-      group_by(date) %>%
-      summarise(count = n())
-    
-    # draw plot
-    ggplot(table, aes(x = date, y = count)) +
-      geom_point(colour = "#d95f02") +
-      geom_line(colour = "#d95f02") +
-      geom_smooth()
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)) %>%
+      addPolygons(data = neighborhood, color = "gray", weight = 2, 
+                  highlight = highlightOptions(
+                    weight = 6, 
+                    color = "#b9aa7e", 
+                    bringToFront = TRUE)) %>%
+      addCircleMarkers(data = housing.units, lng = ~long, lat = ~lat, radius = ~ifelse(type == "single", 4, 8), color = ~palet(type), stroke = FALSE, fillOpacity = .75, label = ~address)
+  })  
+  
+  output$barPlot <- renderPlotly({
+    dat <- cvl.house()
+    ggplotly(
+      ggplot(data = dat, aes(x = neighborhood, y = subsidizedUnits)) + 
+        geom_col(fill = "#8E5572") +
+        labs(title="Six neighborhoods account for 83% of subsidized units", x = "", y="Number of Units") +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))
+      , tooltip = "subsidizedUnits")
   })
-  output$barChart <- renderPlotly({
+  output$lineChart <- renderPlotly({
     dat311 <- load311()
     
     # shape the data for chart
@@ -113,6 +127,23 @@ server <- function(input, output) {
     ggplot(table, aes(x = STATUS, y = count, fill = STATUS)) +
       geom_bar(stat = "identity")
   })
+  # Datatable
+  output$table <- DT::renderDataTable({
+    subset(loadAccount(), select = c(department_name, general_ledger_date, object_account_description, amount))
+  })
+  # Reset Selection of Data
+  observeEvent(input$reset, {
+    updateSelectInput(session, "department_select", selected = c("DPW-Operations"))
+    showNotification("Loading...", type = "message")
+  })
+  # Download data in the datatable
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("revenue.expenses-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(loadAccount(), file)
+    })
 }
 
 # Run the application 
